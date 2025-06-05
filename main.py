4c1861afded248729cf18e066edb3d33472a35b1
Jack Parrigon <jackparrigon@gmail.com>
2025-06-04 19:59:13 -0700

    update stats.py | update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import text_count 
 def main():
-    print (f"{word_count()} words found in the document")
+    print (f"{word_count()} words found in the document", text_count())
 main()
--- stats.py
+++ stats.py
@@ -10,8 +10,38 @@ def word_count():
 
 def text_count():
     text = get_book_text("books/frankenstein.txt")
-    text_split = text.split()
-    letters = text_split.split()
-    lower_letters = letters.lower()
-    now_count = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z":0}
-
+    lower_text = text.lower()
+    low_letters = list(lower_text)
+    letters = " ".join(low_letters)
+    now_count = {"a": 0, 
+                 "b": 0, 
+                 "c": 0, 
+                 "d": 0,
+                 "e": 0,
+                 "f": 0,
+                 "g": 0,
+                 "h": 0,
+                 "i": 0,
+                 "j": 0,
+                 "k": 0,
+                 "l": 0,
+                 "m": 0,
+                 "n": 0,
+                 "o": 0,
+                 "p": 0,
+                 "q": 0,
+                 "r": 0,
+                 "s": 0,
+                 "t": 0,
+                 "u": 0,
+                 "v": 0,
+                 "w": 0,
+                 "x": 0,
+                 "y": 0,
+                 "z":0
+    }
+    for character in now_count:
+        count = now_count[character]
+        if letters == character:
+            count += 1
+        return now_count
