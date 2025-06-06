1a1d8fbe1808337e89c44cdd3c59d9bbb338e663
Jack Parrigon <jackparrigon@gmail.com>
2025-06-05 20:34:10 -0700

    update stats.py w/ with dict sorter

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from stats import word_count
-from stats import text_count 
+from stats import text_count
+from stats import characters_in_order
 def main():
     print ("============ BOOKBOT ============")
     print (f"Analyzing book found at ...")
     print ("----------- Word Count ----------")
     print (f"Found {word_count()} total words")
     print ("--------- Character Count -------")
-    print (f"{text_count()}")
+    print (f"{characters_in_order()}")
     print ("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -12,7 +12,7 @@ def text_count():
     text = get_book_text("books/frankenstein.txt")
     lower_text = text.lower()
     low_letters = list(lower_text)
-    letters = " ".join(low_letters)
+    letters = "".join(low_letters)
     characters = {}
     for i in letters:
         if i not in characters:
@@ -21,8 +21,15 @@ def text_count():
             characters[i] += 1
     return characters
 
-#def characters_in_order:
-#    return None
+def mysort(d):
+    return d["num"]
 
-
-
+def characters_in_order():
+    run_text = text_count()
+    char_sort = []
+    for text in run_text:
+        if text.isalpha():
+            count = run_text[text]
+            char_sort.append({"char": text, "num": count})
+    char_sort.sort(reverse=True, key=mysort)
+    return char_sort
