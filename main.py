c7bb74aba1e37edf8934153cb81b52699e19030d
Jack Parrigon <jackparrigon@gmail.com>
2025-06-05 22:02:55 -0700

    format main to report

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from stats import text_count
 from stats import characters_in_order
 def main():
     print ("============ BOOKBOT ============")
-    print (f"Analyzing book found at ...")
+    print (f"Analyzing book found at books/frankenstein.txt...")
     print ("----------- Word Count ----------")
     print (f"Found {word_count()} total words")
     print ("--------- Character Count -------")
-    print (f"{characters_in_order()}")
+    mychar = characters_in_order()
+    for c in mychar:
+        char = c["char"]
+        num = c["num"]
+        print (f"{char}: {num}")
     print ("============= END ===============")
 main()
